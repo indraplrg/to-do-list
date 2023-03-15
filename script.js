1bb137d9538147ce55b616de177ffc249a10c0fa
indra puluraga <indralolx@gmail.com>
2023-03-15 19:30:13 +0800

    membuat project to do list berbasis web sederhana menggunakan css dan js

---
+++ script.js
@@ -0,0 +1,26 @@
+// Dom
+const input = document.getElementById("input");
+const btn = document.getElementById("btn");
+const todo = document.querySelector(".todo");
+
+// Add List
+btn.addEventListener("click", function () {
+  let li = `<li class='list-style'><span onclick='done(this)'>${input.value}</span><span class='delButton' onclick="remove(this)"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+  <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
+</svg></span></li>`;
+
+  todo.insertAdjacentHTML("afterbegin", li);
+
+  // Restart Field
+  input.value = "";
+});
+
+// Done List
+function done(el) {
+  el.classList.toggle("line-through");
+}
+
+// Remove List
+function remove(el) {
+  el.parentElement.remove();
+}
